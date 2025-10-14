28d681fa3963288b670de4089fcda38bcb54e0c3
pramod <peruripramoddevops@gmail.com>
2025-10-14 21:03:02 +0530

    terraform-2

---
+++ count/variables.tf
@@ -0,0 +1,11 @@
+variable "instances" {
+    default = [ "mongodb", "redis", "mysql", "rabbitmq" ]
+}
+
+variable "zone_id" {
+    default = "Z05714843SO1YQWMF1N5N"
+}
+
+variable "domain_name" {
+    default = "pramod.shop"
+}
